4d4633139df7cf9cf756d7c362f11a0cb0fad40f
ubiZOX <zdh6zox@163.com>
2019-07-25 17:08:22 +0800

    Modify SaveLoadManager and Add 1st version of NameGeneration

--- Scripts/Utils/NameGenerator.cs
+++ Scripts/Utils/NameGenerator.cs
@@ -8,6 +8,9 @@ public class FirstName
     private string m_FirstNameStr;
     [SerializeField]
     private float m_Weight; //用这个来确定姓的出现频率，值越高频率越高
+
+	public float Weight { get { return m_Weight; } set { m_Weight = value; } }
+	public string NameStr { get { return m_FirstNameStr; } set { m_FirstNameStr = value; } }
 }
 
 public class SecondName
@@ -16,25 +19,79 @@ public class SecondName
     private string m_SecondName;
     [SerializeField]
     private GenderType m_Gender;  //名的话，考虑是否是男/女/中性的名字
+
+	public GenderType Gender { get { return m_Gender; } set { m_Gender = value; } }
+	public string NameStr { get { return m_SecondName; } set { m_SecondName = value; } }
 }
 
 public class NameGenerator
 {
     private List<FirstName> m_FirstNamesCache;
     private List<SecondName> m_SecondNameCache;
+
+	private static NameGenerator m_Instance;
     private List<string> m_GeneratedNameList = new List<string>();
 
-    public NameGenerator(string firstNameJson,string secondNameJson)
+    private NameGenerator(string firstNameJson,string secondNameJson)
     {
         m_FirstNamesCache = JsonUtility.FromJson<List<FirstName>>(firstNameJson);
-        m_SecondNameCache = JsonUtility.FromJson<List<SecondName>>(secondNameJson);
-    }
+		if (m_FirstNamesCache.Count == 0)
+			throw new System.Exception("Empty first name table");
+		m_SecondNameCache = JsonUtility.FromJson<List<SecondName>>(secondNameJson);
+		if (m_SecondNameCache.Count == 0)
+			throw new System.Exception("Empty second name table");
+	}
 
-    public bool GenerateNames(int number,ref List<string> nameList)
+	public string GenerateName(GenderType type)
+	{
+		if(m_Instance == null)
+		{
+			m_Instance = new NameGenerator(SaveLoadManager.GetInstance().LoadFirstNameJson(), SaveLoadManager.GetInstance().LoadSecondNameJson());
+		}
+
+		float randomWeight = Random.Range(0, 1);
+		List<FirstName> qualifiedFNList = m_FirstNamesCache.FindAll(n => n.Weight >= randomWeight);
+		List<FirstName> unqualifiedFNList = m_FirstNamesCache.FindAll(n => n.Weight < randomWeight);
+		string finalName = "";
+
+		List<SecondName> qualifiedSNList = m_SecondNameCache.FindAll(n => n.Gender == type || n.Gender == GenderType.GenderType_Unset);
+		List<SecondName> tempSNList = new List<SecondName>(qualifiedSNList.ToArray());
+		while(qualifiedFNList.Count > 0)
+		{
+			int randomFNIndex = Random.Range(0, qualifiedFNList.Count);
+			string firstName = qualifiedFNList[randomFNIndex].NameStr;
+			qualifiedFNList.RemoveAt(randomFNIndex);
+			while(tempSNList.Count > 0)
+			{
+				int randomSNIndex = Random.Range(0, qualifiedSNList.Count);
+				string secondName = tempSNList[randomSNIndex].NameStr;
+				tempSNList.RemoveAt(randomSNIndex);
+				finalName = firstName + secondName;
+				if (m_GeneratedNameList.Exists(n => n == finalName))
+					continue;
+				else
+				{
+					m_GeneratedNameList.Add(firstName);
+					return finalName;
+				}
+			}
+		}
+
+		//cannot generate name from qualified first name, do it in unqualified list
+
+		throw new System.Exception("Cannot generate name anymore!");
+		return "";
+	}
+
+    public List<string> GenerateNames(int number)
     {
-        
-        if (m_GeneratedNameList.Count == number)
-            return true;
-        return false;
+		for(int i =0;i<number; ++i)
+		{
+			float randomWeight = Random.Range(0, 1);
+			List<FirstName> qualifiedList = m_FirstNamesCache.FindAll(n => n.Weight >= randomWeight);
+			int randomIndex = Random.Range(0, qualifiedList.Count - 1);
+			string firstName = qualifiedList[randomIndex].NameStr;
+		}
+		return m_GeneratedNameList;
     }
 }
--- Scripts/Utils/SaveLoadManager.cs
+++ Scripts/Utils/SaveLoadManager.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class SaveLoadManager : MonoBehaviour
 {
-    private string m_SaveFilePath;
-    private string m_CustomerTablePath;
-    private string m_FirstNameTablePath;
-    private string m_SecondNameTablePath;
+    private static string m_SaveFilePath;
+    private static string m_CustomerTablePath;
+    private static string m_FirstNameTablePath;
+    private static string m_SecondNameTablePath;
+
+	private static SaveLoadManager m_Instance;
     
     // Start is called before the first frame update
     void Start()
@@ -24,6 +27,14 @@ public class SaveLoadManager : MonoBehaviour
         
     }
 
+	private SaveLoadManager(string dataPath,string productName)
+	{
+		m_SaveFilePath = dataPath + "/" + productName + "/Save" + "/save.json";
+		m_CustomerTablePath = dataPath + "/" + productName + "/Save" + "/Customers.json";
+		m_FirstNameTablePath = dataPath + "/" + productName + "/Temp" + "/FirstName.json";
+		m_SecondNameTablePath = dataPath + "/" + productName + "/Temp" + "/SecondName.json";
+	}
+
     public void SaveGame()
     {
         
@@ -33,4 +44,36 @@ public class SaveLoadManager : MonoBehaviour
     {
 
     }
+
+	public static SaveLoadManager GetInstance()
+	{
+		if (m_Instance == null)
+		{
+			m_Instance = new SaveLoadManager(Application.dataPath, Application.productName);
+		}
+
+		return m_Instance;
+	}
+
+	public string LoadFirstNameJson()
+	{
+		if(File.Exists(m_FirstNameTablePath))
+		{
+			return File.ReadAllText(m_FirstNameTablePath);
+		}
+
+		throw new System.Exception("Cannot Find First Name Table");
+		return "";
+	}
+
+	public string LoadSecondNameJson()
+	{
+		if (File.Exists(m_SecondNameTablePath))
+		{
+			return File.ReadAllText(m_SecondNameTablePath);
+		}
+
+		throw new System.Exception("Cannot Find Second Name Table");
+		return "";
+	}
 }
